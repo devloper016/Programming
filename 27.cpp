07b2524cfbffe9029362dc8c6d9317fccd6a70be
Aditya <85797633+devloper016@users.noreply.github.com>
2022-02-12 18:10:41 +0530

    Add files via upload

---
+++ 27.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;++i){
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    int val;
+    cin>>val;
+    int c=0;
+    for(int i=0;i<n;++i){
+        if(v[i]==val){
+            c++;
+        }
+    }
+    v.erase(v.end()-c,v.end());
+    cout<<v.size();
+}
