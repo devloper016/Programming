1e9210b5d6c7e6b8637737ca506b25139a66a8e5
Aditya <85797633+devloper016@users.noreply.github.com>
2022-02-23 19:41:42 +0530

    Create 32.cpp

---
+++ 32.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s = 0, e = nums.size() - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>=nums[s]){
+                if(nums[mid]>target&& target>=nums[s]){
+                    e = mid-1;
+                }else{
+                    s = mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target&&target<=nums[e]){
+                    s = mid+1;
+                }
+                else{
+                    e = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
