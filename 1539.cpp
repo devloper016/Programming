8c4ab7a650e4b8404ba89f96d2f6f47af2366e3a
Aditya <85797633+devloper016@users.noreply.github.com>
2022-02-26 19:54:48 +0530

    Create 1539.cpp

---
+++ 1539.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+     int s=0 , e=arr.size()-1,mid;
+     while(s<=e){
+         mid = s + (e-s)/2;
+         if((arr[mid]-mid-1)<k){
+             s = mid+1;
+         }
+         else{
+             e = mid-1;
+         }
+     }
+        return s+k;
+    }
+};
