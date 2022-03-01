2b80fe5ab0dce448ce36ee9580bbce94e65c15e6
Aditya <85797633+devloper016@users.noreply.github.com>
2022-03-01 22:35:16 +0530

    Create batterylow.cpp

---
+++ batterylow.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x>15){
+            cout<<"No"<<endl;
+        }
+        else if(x<=15){
+            cout<<"Yes"<<endl;
+        }
+    }
+}
