43c26540d0f7377bf9433f298a52803b83ba499a
Aditya <85797633+devloper016@users.noreply.github.com>
2022-03-01 22:34:42 +0530

    Create Distcon.cpp

---
+++ Distcon.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+        int d;
+        cin>>d;
+        if(n%2!=0){
+        cout<<0<<" "<<(d/2)<<endl;
+        cout<<(d/2)<<" "<<0<<endl;
+        cout<<0<<" "<< -(d/2)<<endl;
+        cout<<-(d/2)<<" "<<0<<endl;
+    }
+    cout<<-1<<endl;
+}
