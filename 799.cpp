412b45d025e7145905854e1665e1004cbe8eead4
Aditya <85797633+devloper016@users.noreply.github.com>
2022-03-04 19:01:37 +0530

    Create 799.cpp

---
+++ 799.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<double> ans(101);
+        ans[0] = poured;
+        for (int row = 1; row <= query_row; row++)
+            for (int i = row; i >= 0; i--) {
+                ans[i] = max(0.0, (ans[i] - 1) / 2);
+                ans[i + 1] += ans[i];
+            }
+                
+        return min(ans[query_glass], 1.0);
+    }
+};
