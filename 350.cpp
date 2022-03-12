f156465383d573b4d366bd01cce8a577bfba47f5
Aditya <85797633+devloper016@users.noreply.github.com>
2022-03-12 11:47:31 +0530

    Create 350.cpp

---
+++ 350.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        vector<int> res;
+        for(int i=0;i<nums1.size();i++){
+            int s=0,e=nums2.size()-1;
+            while(s<=e && nums2.size()!=0){
+                int mid = s + (e-s)/2;
+                if(nums2[mid]==nums1[i]){
+                    res.push_back(nums2[mid]);
+                    nums2.erase(nums2.begin()+mid);
+                    break;
+                }
+                else if(nums2[mid]>nums1[i]) e = mid-1;
+                else{
+                    s = mid+1;
+                }
+            }
+        }
+        return res;
+    }
+};
