fe7c0dd50fa5bdb87e360a1cec5f29c900f218c8
Aditya <85797633+devloper016@users.noreply.github.com>
2022-03-28 17:39:53 +0530

    Create chefraces.cpp

---
+++ chefraces.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,a,b;
+        cin>>x>>y>>a>>b;
+        if(((x==a) || (x==b)) && ((y==a) || (y==b))){
+            cout<<0<<endl;
+        }
+        else if(((x==a) || (x==b))|| ((y==a) || (y==b))){
+            cout<<1<<endl;
+        }
+        else{
+            cout<<2<<endl;
+        }
+    }
+}
