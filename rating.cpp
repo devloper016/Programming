0141641fbdbacd9340ffb0eba4ddb05b0096f71b
Aditya <85797633+devloper016@users.noreply.github.com>
2022-03-28 17:39:23 +0530

    Create rating.cpp

---
+++ rating.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int t;
+ cin>>t;
+ while(t--)
+ {
+     int s;
+     cin>>s;
+     int x=1;
+     int ans = -(s+x);
+     cout<<ans<<endl;
+ }
+ return 0;
+}
