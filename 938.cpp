6efc18c234fb18c6e19b00c1deb7a028365f529b
Aditya <85797633+devloper016@users.noreply.github.com>
2022-04-20 00:36:05 +0530

    Create 938.cpp

---
+++ 938.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root==NULL) return 0;
+        int sum=0;
+        if(root->val>=low && root->val <= high){
+            sum += root->val;
+        }
+        if(root->val > high){
+            sum += rangeSumBST(root->left,low,high);
+        }
+        else if(root->val < low){
+            sum += rangeSumBST(root->right,low,high);
+        }
+        else{
+            sum += rangeSumBST(root->left,low,high) + rangeSumBST(root->right,low,high);
+        }
+        return sum;
+    }
+};
