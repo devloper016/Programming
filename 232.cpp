c7c5dd554f63d83967817a32de7376a7d5eb48ed
Aditya <85797633+devloper016@users.noreply.github.com>
2022-06-10 16:56:36 +0530

    Create 232.cpp

---
+++ 232.cpp
@@ -0,0 +1,55 @@
+class MyQueue {
+public:
+    stack<int> s1;
+    stack<int> s2;
+    MyQueue() {
+        return;
+    }
+    
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    int pop() {
+        int ans=-1;
+        if(!s2.empty()){
+            ans = s2.top();
+            s2.pop();
+            return ans;
+        }
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+        ans = s2.top();
+        s2.pop();
+        return ans;
+    }
+    
+    int peek() {
+        int ans=-1;
+        if(!s2.empty()){
+            ans = s2.top();
+            return ans;
+        }
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+        ans = s2.top();
+        return ans;
+    }
+    
+    bool empty() {
+        return (s1.empty() && s2.empty());
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
