8f78a0c0d0008e67201f691f3fb1cb367a396a37
Aditya <85797633+devloper016@users.noreply.github.com>
2022-06-11 12:20:11 +0530

    Create 463.cpp

---
+++ 463.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int ans=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    ans+=4;
+                    if(i>0&&grid[i-1][j]==1)ans--;
+                    if(i+1<grid.size()&&grid[i+1][j]==1)ans--;
+                    if(j>0&&grid[i][j-1]==1)ans--;
+                    if(j+1<grid[0].size()&&grid[i][j+1]==1)ans--;
+                }
+            }
+        }
+        return ans;
+    }
+};
