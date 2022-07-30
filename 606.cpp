6bcfb7208672e967a538cd05474d0357de131c7c
Aditya <85797633+devloper016@users.noreply.github.com>
2022-07-30 11:53:38 +0530

    Create 606.cpp

---
+++ 606.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        if(!root) return "";
+        
+        if(!root->left and!root->right) return to_string(root->val);
+        
+        if(root->right==NULL){
+            return to_string(root->val)+"("+tree2str(root->left)+")";
+        }
+        return to_string(root->val)+"("+tree2str(root->left)+")"+"("+tree2str(root->right)+")";
+    }
+};
