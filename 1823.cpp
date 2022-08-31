e8e52641ac39baae9b3b18c80f03ca943b6c9d51
Aditya <85797633+devloper016@users.noreply.github.com>
2022-08-31 16:44:31 +0530

    Create 1823.cpp

---
+++ 1823.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int sol(int n,int k){
+        if(n==1) return 0;
+        
+        return ((sol(n-1,k)+k)%n);
+    }
+    int findTheWinner(int n, int k) {
+       return sol(n,k)+1;
+    }
+};
