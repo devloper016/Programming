0c4bb9234f532ff0db2151165fb105c4aab69fd9
Aditya <85797633+devloper016@users.noreply.github.com>
2022-09-04 13:29:38 +0530

    Create 987.cpp

---
+++ 987.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    map<int,map<int,multiset<int>>> mp;
+    void dfs(TreeNode* root,int col,int row){
+        if(!root){
+            return;
+        }
+        mp[col][row].insert(root->val);
+        dfs(root->left,col-1,row+1);
+        dfs(root->right,col+1,row+1);
+    }
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> ans;
+        // if(!root){
+        //     return ans;
+        // }
+        dfs(root,0,0);
+        for(auto m1 : mp){
+            ans.push_back(vector<int>());
+            for(auto m2 : m1.second){
+                for(auto m3 : m2.second){
+                    ans.back().push_back(m3);
+                }
+            }
+        }
+        return ans;
+    }
+};
