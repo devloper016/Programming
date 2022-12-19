9a72112336a5b78d03c63a1d5d17346c5c92c1cc
Aditya <adityagoku613@gmail.com>
2022-12-19 19:59:53 +0530

    Create 1971.cpp

---
+++ 1971.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool ifPath(int source,int destination,vector<bool>& vis,vector<int> adj[])
+    {
+        vis[source]=true;                               
+        cout<<source<<" ";
+        if(source==destination) return true;            
+        for(auto it : adj[source])
+        {
+            if(!vis[it])
+            {
+                if(ifPath(it,destination,vis,adj))
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) 
+    {
+        if(source==destination) return true;
+        
+    
+        vector<int> adj[n];
+        for(int i=0;i<edges.size();i++)
+        {
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+        
+        vector<bool> vis(n,false);
+        
+        
+        if(ifPath(source,destination,vis,adj))
+        {
+            return true;
+        }
+        return false;
+    }
+};
