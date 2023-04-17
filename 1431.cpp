ee01dcb449a287e07daa50dd4993e2ac3feebb3b
Aditya <adityagoku613@gmail.com>
2023-04-17 22:13:35 +0530

    Create 1431.cpp

---
+++ 1431.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int mx = *max_element(candies.begin(),candies.end());
+        vector<bool> ans;
+        int n = candies.size();
+        for(int i=0;i<n;i++){
+            if(candies[i]+extraCandies>=mx) ans.push_back(true);
+            else ans.push_back(false);
+        }
+        return ans;
+    }
+};
