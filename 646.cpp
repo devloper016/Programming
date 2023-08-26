1382eef810912468bde9812087051510406ad0df
Aditya <adityagoku613@gmail.com>
2023-08-26 20:20:51 +0530

    Create 646.cpp

---
+++ 646.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findLongestChain(vector<vector<int>>& pairs) {
+        int ans=0;
+        int n = pairs.size();
+        vector<int> dp(n,1);
+        sort(pairs.begin(),pairs.end());
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                if(pairs[i][1]<pairs[j][0]){
+                    dp[i] = max(dp[i],1+dp[j]);
+                }
+            }
+            ans = max(ans,dp[i]);
+        }
+        return ans;
+    }
+};
