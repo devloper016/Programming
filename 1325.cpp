3bdd8cf143f80af0a0748572f655c852eac14ba5
Aditya Tiwari <adityagoku613@gmail.com>
2024-01-29 19:25:41 +0530

    Create 1325.cpp

---
+++ 1325.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* &root,int target){
+        if(root==NULL) return;
+        dfs(root->left,target);
+        dfs(root->right,target);
+        if(root->left==NULL && root->right==NULL && root->val==target){
+            root=NULL;
+        }
+        return;
+    }
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+       dfs(root,target);
+       return root;
+    }
+};
