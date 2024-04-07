101f8d4ee554c2631ab755734793bf34297eded7
Aditya Tiwari <adityagoku613@gmail.com>
2024-04-07 12:39:30 +0530

    Create 678.cpp

---
+++ 678.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n= s.size();
+        stack<int> st;
+        stack<int> star;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                st.push(i);
+            }
+            else if(s[i]=='*'){
+                star.push(i);
+            }
+            else if(s[i]==')'){
+                if(st.empty() && star.empty()) return false;
+                else if(!st.empty()) st.pop();
+                else star.pop();
+            }
+        }
+        while(!st.empty()){
+            if(star.empty()) return false;
+            if(st.top()>star.top()) return false;
+            st.pop();
+            star.pop();
+        }
+        return true;
+    }
+};
